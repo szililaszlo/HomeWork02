fb0544ad0af69906daa425041278c8c0bb24221d
szililaszlo <laszlo0919@gmail.com>
2017-12-04 00:54:17 +0100

    #3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import com.company.homework02.CalculationHelper;
 import com.company.homework02.CalculatorService;
 
 import java.util.Scanner;
@@ -32,6 +33,26 @@ public class Main {
         char task2Operation = task2NewOperation.next().charAt(0);;
         //a függvény meghívása a bekért adatokkal
         System.out.println(CalculatorService.calculate(task2Number1,task2Number2,task2Operation));
+        System.out.println("-------------------------------");
+
+        //3
+        //a 2. feladatot is már saját értékek bekérésvel  készítettem el, ezért ezt a feladatot nem módósítottam ahhoz
+        // képest csupán csak a class nevét és a változókat írtam át
+        //számok bekérése
+        System.out.println("Kérek egy egész számot: ");
+        Scanner task3NewNumber1 = new Scanner(System.in);
+        System.out.println("Kérek ismét egy egész számot: ");
+        Scanner task3NewNumber2 = new Scanner(System.in);
+        //műveleti jel bekérése
+        System.out.println("Kérek egy műveleti jelet: ");
+        Scanner task3NewOperation = new Scanner(System.in);
+        //szám változóba mentése
+        int task3Number1 = task3NewNumber1.nextInt();
+        int task3Number2 = task3NewNumber2.nextInt();
+        //a bevitt szöveg 1 karakterének imentése( műveleti jel lementése változóba)
+        char task3Operation = task3NewOperation.next().charAt(0);;
+        //a függvény meghívása a bekért adatokkal
+        System.out.println(CalculationHelper.calculate(task3Number1,task3Number2,task3Operation));
         System.out.println("-------------------------------");
     }
 }
---
+++ src/com/company/homework02/CalculationHelper.java
@@ -0,0 +1,26 @@
+package com.company.homework02;
+
+public class CalculationHelper {
+    // publikus és statikus metódus lértehozása, hogy a Main függvényből meghívható legyen
+    public static int calculate(int num1, int num2, char operation) {
+        //eredmény tárolására szolgáló változó
+        int result;
+        // műveleti jel alapján történő művelet elvégzés
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            default :
+                result = num1 / num2;
+                break;
+        }
+        //visszatérési érték
+        return result;
+    }
+}
