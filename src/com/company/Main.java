ceb142f149b93ac3e7464f7c824a6f0fd0727995
szililaszlo <laszlo0919@gmail.com>
2017-12-04 00:46:05 +0100

    #2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,5 +15,23 @@ public class Main {
         //a függvény meghívása a bekért számmal
         System.out.println(CalculatorService.isItEven(number));
         System.out.println("-------------------------------");
+
+        //2
+        //számok bekérése
+        System.out.println("Kérek egy egész számot: ");
+        Scanner task2NewNumber1 = new Scanner(System.in);
+        System.out.println("Kérek ismét egy egész számot: ");
+        Scanner task2NewNumber2 = new Scanner(System.in);
+        //műveleti jel bekérése
+        System.out.println("Kérek egy műveleti jelet: ");
+        Scanner task2NewOperation = new Scanner(System.in);
+        //szám változóba mentése
+        int task2Number1 = task2NewNumber1.nextInt();
+        int task2Number2 = task2NewNumber2.nextInt();
+        //a bevitt szöveg 1 karakterének imentése( műveleti jel lementése változóba)
+        char task2Operation = task2NewOperation.next().charAt(0);;
+        //a függvény meghívása a bekért adatokkal
+        System.out.println(CalculatorService.calculate(task2Number1,task2Number2,task2Operation));
+        System.out.println("-------------------------------");
     }
 }
--- src/com/company/homework02/CalculatorService.java
+++ src/com/company/homework02/CalculatorService.java
@@ -2,7 +2,7 @@ package com.company.homework02;
 
 public class CalculatorService {
 
-    // publikus és ttatikus metódus lértehozása, hogy a Main függvényből meghívható legyen
+    // publikus és statikus metódus lértehozása, hogy a Main függvényből meghívható legyen
     public static boolean isItEven(int number) {
         // if feltételes elágazás ami kiértékeli, ohgy a szám párpos-e
         if (number % 2 == 0) {
@@ -10,6 +10,29 @@ public class CalculatorService {
         } else {
             return false;
         }
+    }
+
+    // publikus és statikus metódus lértehozása, hogy a Main függvényből meghívható legyen
+    public static int calculate(int num1, int num2, char operation) {
+        //eredmény tárolására szolgáló változó
+        int result;
+        // műveleti jel alapján történő művelet elvégzés
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            default :
+                result = num1 / num2;
+                break;
+        }
+        //visszatérési érték
+        return result;
     }
 
 }
